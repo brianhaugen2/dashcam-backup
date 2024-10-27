f2e3fde7cfdc9f0de127801b87058e45a41c9121
Brian Haugen <brian.haugen@toyota.com>
2024-10-27 18:53:11 -0400

    typo

--- scripts/conversion.sh
+++ scripts/conversion.sh
@@ -1,10 +1,11 @@
 #!/usr/bin/env bash
 
-docker run -d \
+docker container rm rlog-conversion && \
+docker run -it \
+--entrypoint bash \
 --name rlog-conversion \
 -v /media:/media \
 -v /home/brian/repos/dashcam-backup:/tmp/dashcam-backup \
 ghcr.io/commaai/openpilot-prebuilt \
 cd /tmp/dashcam-backup && \
-pip install . && \
 python scripts/rlog_to_parquet.py
--- scripts/docker_commands.sh
+++ scripts/docker_commands.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
 
 git pull
-docker build -t dash-backup:latest .
+docker build -t dashcam-backup:latest .
 docker kill dashcam-backup
 docker rm dashcam-backup
 docker run -d --name dashcam-backup --restart always -v /home/brian/.ssh:/home/brian/.ssh -v /media/:/media/ dashcam-backup:latest python dashcam_backup/main.py
