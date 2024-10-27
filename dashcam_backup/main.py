65f0fd7f1c24aedd73ace2b996eb502aac5a217e
Brian Haugen <brian.haugen@toyota.com>
2024-10-27 19:36:15 -0400

    don't install external dependencies on comma

--- dashcam_backup/main.py
+++ dashcam_backup/main.py
@@ -75,7 +75,8 @@ def main():
         # update code on the device before running the backup
         subprocess.run([
             "ssh", COMMA_IP,
-            "cd /data/media/0/dashcam-backup && git pull && pip install ."
+            "cd /data/media/0/dashcam-backup && git pull "
+            "&& pip install . --no-deps"
         ])
 
         if os.path.exists(BACKUP_CATALOG_FP):
