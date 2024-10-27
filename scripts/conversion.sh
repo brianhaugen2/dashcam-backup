de20f19027bfab7c3914fbb44051a640ac8a667b
Brian Haugen <brian.haugen@toyota.com>
2024-10-27 01:00:41 -0400

    removing some dependencies

--- scripts/conversion.sh
+++ scripts/conversion.sh
@@ -4,7 +4,7 @@ docker run -d \
 --name rlog-conversion \
 -v /media:/media \
 -v /home/brian/repos/dashcam-backup:/tmp/dashcam-backup \
-commaai-conversion \
+ghcr.io/commaai/openpilot-prebuilt \
 cd /tmp/dashcam-backup && \
 pip install . && \
 python scripts/rlog_to_parquet.py
--- scripts/rlog_to_parquet.py
+++ scripts/rlog_to_parquet.py
@@ -1,27 +1,25 @@
 import os
-import pandas as pd
+import pickle as pkl
 
 from tools.lib.logreader import LogReader
-from dashcam_backup.params import (
-    BACKUP_CATALOG_FP,
-)
 
 
 if __name__ == "__main__":
-    cat = pd.read_csv(BACKUP_CATALOG_FP)
-    rlog = cat[cat["local_path"].str.endswith("rlog")]
-    for _, row in rlog.iterrows():
-        raw_fp = row["local_path"]
-        conv_fp = raw_fp.replace("raw_data", "converted_data").replace("rlog", "rlog.parquet")
-        if not os.path.exists(conv_fp):
-            os.makedirs(os.path.dirname(conv_fp), exist_ok=True)
-            lr = LogReader(raw_fp)
-            out = []
-            for msg in lr:
-                try:
-                    dict_msg = msg.to_dict()
-                    out.append(dict_msg)
-                except UnicodeDecodeError:
-                    continue
-            df = pd.DataFrame(out)
-            df.to_parquet(conv_fp)
+    for root, dirs, in os.walk("/media/brian/7ef70690-cfa7-4aea-9ae2-cb402f50dec0/dashcam_backup/raw_data"):
+        for d in dirs:
+            raw_fp = os.path.join(root, d, "rlog")
+            conv_fp = raw_fp.replace("raw_data", "converted_data").replace("rlog", "rlog.pkl")
+            if not os.path.exists(conv_fp) and os.path.exists(raw_fp):
+                print(raw_fp)
+                os.makedirs(os.path.dirname(conv_fp), exist_ok=True)
+                lr = LogReader(raw_fp)
+                out = []
+                for msg in lr:
+                    try:
+                        dict_msg = msg.to_dict()
+                        out.append(dict_msg)
+                    except UnicodeDecodeError:
+                        continue
+
+                with open(conv_fp, "wb") as f:
+                    pkl.dump(out, f)
