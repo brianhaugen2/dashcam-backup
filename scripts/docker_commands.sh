75d2757b9969baa36838d1933f50cb56cfddd9e3
Brian Haugen <brian.haugen@toyota.com>
2024-10-26 23:45:09 -0400

    typo

--- scripts/docker_commands.sh
+++ scripts/docker_commands.sh
@@ -2,6 +2,6 @@
 
 git pull
 docker build -t dash-backup:latest .
-docker kill dash-backup
-docker rm dash-backup
+docker kill dashcam-backup
+docker rm dashcam-backup
 docker run -d --name dashcam-backup --restart always -v /home/brian/.ssh:/home/brian/.ssh -v /media/:/media/ dashcam-backup:latest python dashcam_backup/main.py
