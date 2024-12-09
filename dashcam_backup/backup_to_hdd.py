132db7ee74613a5366f3cb1124c09807196c72ab
Brian Haugen <bhaugen2@gmail.com>
2024-12-08 20:19:22 -0500

    capture errors

--- dashcam_backup/backup_to_hdd.py
+++ dashcam_backup/backup_to_hdd.py
@@ -20,17 +20,20 @@ def main():
     while True:
         logger.info(f"Loop {i}")
         start_t = time.time()
-        result = subprocess.run(
-            ["rsync", f"{COMMA_IP}:{COMMA_DATA_DIR}/*", f"{RAW_DATA_DIR}", "-av"],
-            stdout=subprocess.PIPE,
-            stderr=subprocess.PIPE,
-            text=True,
-            check=True
-        )
+        try:
+            result = subprocess.run(
+                ["rsync", f"{COMMA_IP}:{COMMA_DATA_DIR}/*", f"{RAW_DATA_DIR}", "-av"],
+                stdout=subprocess.PIPE,
+                stderr=subprocess.PIPE,
+                text=True,
+                check=True
+            )
 
-        logger.info(result.stdout)
-        if result.stderr:
-            logger.error(result.stderr)
+            logger.info(result.stdout)
+            if result.stderr:
+                logger.info(result.stderr)
+        except Exception as e:
+            logger.info(e)
 
         if (time.time() - start_t) < 60:
             logger.info(f"Sleeping for {60 - time.time() + start_t} sec")
